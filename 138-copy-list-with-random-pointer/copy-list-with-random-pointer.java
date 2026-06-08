5e98b19a1616c186e9adef4e0ff66e2a1f315a29
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-06-08 20:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (69.50%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,22 +15,54 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+        // Node temp1 = head;
+        // Map<Node,Node> map = new HashMap<>();
+        // while(temp1!=null){
+        //     Node newNode = new Node(temp1.val);
+        //     map.put(temp1,newNode);
+        //     temp1 = temp1.next;
+        // }
+
+        // Node temp2 = head;
+
+        // while(temp2!=null){
+        //     Node copyNode = map.get(temp2);
+        //     copyNode.next = map.get(temp2.next);
+        //     copyNode.random = map.get(temp2.random);
+        //     temp2 = temp2.next;
+        // }
+        // return map.get(head); // Sol1
+
+        Node temp = head;
+        //Step1 create new copy node 
+        while(temp!=null){
+            Node copyNode = new Node(temp.val);
+            copyNode.next =temp.next;
+            temp.next = copyNode;
+            temp = temp.next.next;
+        }
+        //step2 connecting random pntr
         Node temp1 = head;
-        Map<Node,Node> map = new HashMap<>();
         while(temp1!=null){
-            Node newNode = new Node(temp1.val);
-            map.put(temp1,newNode);
-            temp1 = temp1.next;
+            Node copyNode = temp1.next;
+            if(temp1.random!=null)
+             copyNode.random = temp1.random.next;
+             else
+                copyNode.random = null;
+            temp1 = temp1.next.next;
         }
 
+        //Step3 connecting next ptr
+        Node dummy = new Node(-1);
+        Node res = dummy;
         Node temp2 = head;
-
         while(temp2!=null){
-            Node copyNode = map.get(temp2);
-            copyNode.next = map.get(temp2.next);
-            copyNode.random = map.get(temp2.random);
+            res.next = temp2.next;
+            temp2.next = temp2.next.next;
+            res = res.next;
             temp2 = temp2.next;
         }
-        return map.get(head);
+
+        return dummy.next;
     }
 }
