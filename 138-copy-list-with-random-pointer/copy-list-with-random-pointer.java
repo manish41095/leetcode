0ed4042745b273f205d385da7c8b253dd6e6484d
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-06-08 21:30:53 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (13.57%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -33,36 +33,37 @@ class Solution {
         // }
         // return map.get(head); // Sol1
 
-        Node temp = head;
-        //Step1 create new copy node 
-        while(temp!=null){
-            Node copyNode = new Node(temp.val);
-            copyNode.next =temp.next;
-            temp.next = copyNode;
-            temp = temp.next.next;
+       if(head == null)
+        return null;
+        Node curr = head;
+        //create new list
+        while(curr!=null){
+            Node newNode = new Node(curr.val);
+            newNode.next = curr.next;
+            curr.next = newNode;
+            curr = newNode.next;
         }
-        //step2 connecting random pntr
-        Node temp1 = head;
-        while(temp1!=null){
-            Node copyNode = temp1.next;
-            if(temp1.random!=null)
-             copyNode.random = temp1.random.next;
-             else
-                copyNode.random = null;
-            temp1 = temp1.next.next;
+        curr = head;
+        //Copy Random ptr;
+        while(curr!=null){
+            if(curr.random!=null)
+                curr.next.random = curr.random.next;
+
+            curr = curr.next.next;
         }
 
-        //Step3 connecting next ptr
-        Node dummy = new Node(-1);
-        Node res = dummy;
-        Node temp2 = head;
-        while(temp2!=null){
-            res.next = temp2.next;
-            temp2.next = temp2.next.next;
-            res = res.next;
-            temp2 = temp2.next;
-        }
+        curr = head;
+        Node newHead = head.next;
+        Node newCurr = newHead;
+        //Separate two list
+        while(curr!=null){
+            curr.next = newCurr.next;
+            curr = curr.next;
 
-        return dummy.next;
+            if(curr!=null)
+            newCurr.next = curr.next;
+            newCurr = newCurr.next;
+        }
+        return newHead;
     }
 }
