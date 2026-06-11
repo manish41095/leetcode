cd9ee6e370526ab650a64f38457ac02a73c907f2
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-06-11 23:13:23 +0530

    Time: 3 ms (78.27%) | Memory: 42.7 MB (95.88%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -1,31 +1,49 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        int start = 0;
-        int end = 1;
-        int currNum;
-        List<String> ranges = new ArrayList<>();
         int n = nums.length;
-        if(n==0)
-         return ranges;
-       
-        while(start<n){
-            currNum = nums[start];
-            while(end<n && nums[end]==currNum+1){
-                currNum = nums[end];
-                end++;
+        List<String> ranges = new ArrayList<>();
+        int start;
+        for(int i = 0; i < n; i++ ){
+            start=nums[i];
+            while(i+1<n && nums[i+1] == nums[i]+1){
+                i++;
             }
-
             String str = null;
-            if (start + 1 == end) {
-                str = String.valueOf(nums[start]);
-            } else {
-                str = nums[start] + "->" + nums[end - 1];
+            if(start!=nums[i]){
+                str = start + "->" + nums[i];
             }
-            ranges.add(str);
-            start = end;
+            else{
+                str = String.valueOf(start);
+            }
 
-            end++;
+            ranges.add(str);
         }
-      return ranges;  
+    //     int start = 0;
+    //     int end = 1;
+    //     int currNum;
+    //     List<String> ranges = new ArrayList<>();
+    //     int n = nums.length;
+    //     if(n==0)
+    //      return ranges;
+       
+    //     while(start<n){
+    //         currNum = nums[start];
+    //         while(end<n && nums[end]==currNum+1){
+    //             currNum = nums[end];
+    //             end++;
+    //         }
+
+    //         String str = null;
+    //         if (start + 1 == end) {
+    //             str = String.valueOf(nums[start]);
+    //         } else {
+    //             str = nums[start] + "->" + nums[end - 1];
+    //         }
+    //         ranges.add(str);
+    //         start = end;
+
+    //         end++;
+    //     }
+       return ranges;  
     }
 }
