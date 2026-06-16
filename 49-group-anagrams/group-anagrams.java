10bdf27897d20005c789260efe2c06e180701991
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-06-16 23:36:30 +0530

    Time: 15 ms (26.50%) | Memory: 49.9 MB (35.65%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,12 +1,29 @@
 class Solution {
+    public String getCharCount(String s){
+        int[] charCount = new int[26];
+        char[] sToChar = s.toCharArray();
+        for(char c : s.toCharArray()){
+            charCount[c - 'a' ]++;
+        }
+
+        StringBuilder sb = new StringBuilder("");
+        char c = 'a';
+        for(int i : charCount){
+            sb.append(c);
+            sb.append(i);
+            c++;
+        }
+        return sb.toString();
+    }
     public List<List<String>> groupAnagrams(String[] strs) {
 
         Map<String, List<String>> res = new HashMap<>();
 
         for(int i = 0; i < strs.length ; i++){
-            char[] strToChar = strs[i].toCharArray();
-            Arrays.sort(strToChar);
-            String sortedStr = new String(strToChar);
+            //char[] strToChar = strs[i].toCharArray();
+           // Arrays.sort(strToChar);
+          //  String sortedStr = new String(strToChar);
+            String sortedStr = getCharCount(strs[i]);
             if(res.containsKey(sortedStr))
             res.get(sortedStr).add(strs[i]);
             else{
