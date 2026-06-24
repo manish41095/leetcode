0524cc28a19a00d8779a1b3508bacebbf303e13c
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-06-24 23:25:48 +0530

    Time: 6 ms (89.90%) | Memory: 57.7 MB (61.71%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,16 +1,28 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
        int n = nums.length;
-       Set<Integer> set = new HashSet<>();
-       List<Integer> result  = new ArrayList<>();
-       for(int i = 0 ; i < n ; i ++){
-        if(set.contains(nums[i])){
-            result.add(nums[i]);
+    // Method 1 using extra space SC : O(n) , TC: O(n)  
+    //    Set<Integer> set = new HashSet<>();
+    //    List<Integer> result  = new ArrayList<>();
+    //    for(int i = 0 ; i < n ; i ++){
+    //     if(set.contains(nums[i])){
+    //         result.add(nums[i]);
+    //     }
+    //     else{
+    //         set.add(nums[i]);
+    //     }
+    //    }
+    //    return result;
+
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
+            int index = Math.abs(nums[i])-1;
+
+            if(nums[index]<0)
+            result.add(Math.abs(nums[i]));
+
+            nums[index] = nums[index]*(-1);
         }
-        else{
-            set.add(nums[i]);
-        }
-       }
-       return result;
+        return result;
     }
 }
