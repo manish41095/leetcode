c789d3fbfd2842f78878f5b063de27254b718d9f
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-06-29 22:49:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (54.18%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    //  public int factorial(int n){
+    //         int num=1;
+    //         for(int i = n; i>0; i--){
+    //             num = num*i;
+    //         }
+    //       return num;
+    //     }
+    public int trailingZeroes(int n) {
+    //    int fact = factorial(n);
+    //    int count = 0;
+    //    while(fact%10==0){
+    //     count++;
+    //     fact= fact/10;
+    //    }
+    int count = 0;
+    for (int i = 5; n / i >= 1; i *= 5) {
+        count += n / i;
+    }
+    return count;
+    }
+}
