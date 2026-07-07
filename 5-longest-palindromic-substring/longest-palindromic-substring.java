e68f29dfe8eef2b630c9b13ad5c91f545e676391
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-07-07 23:06:03 +0530

    Time: 1665 ms (9.34%) | Memory: 43.3 MB (89.29%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -20,16 +20,17 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 if (isPalnidrome(s, i, j)) {
-
-                    if ((j - i + 1) > max) {
-                        max = j - i + 1;
+                    int len = j - i + 1;
+                    if ( len > max) {
+                        max = len;
                         start = i;
                         end = j;
                     }
+                    
                 }
 
             }
         }
-        return s.substring(start, end + 1);
+        return s.substring(start, start+max);
     }
 }
