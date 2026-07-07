1f943cbdcee9313a920b85a8fdfce5ea06f72e15
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-07-08 00:23:35 +0530

    Time: 13 ms (96.33%) | Memory: 43.5 MB (70.10%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,34 +1,62 @@
 class Solution {
-    public boolean isPalnidrome(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            } else {
-                i++;
-                j--;
-            }
-        }
-        return true;
-    }
+
+    // public boolean isPalnidrome(String s, int i, int j) {
+    //     while (i < j) {
+    //         if (s.charAt(i) != s.charAt(j)) {
+    //             return false;
+    //         } else {
+    //             i++;
+    //             j--;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    // public String longestPalindrome(String s) {
+    //     //Without DP;
+    //     int max = 0;
+    //     int start = 0;
+    //     int n = s.length();
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = i; j < n; j++) {
+    //             if (isPalnidrome(s, i, j)) {
+    //                 int len = j - i + 1;
+    //                 if ( len > max) {
+    //                     max = len;
+    //                     start = i;
+    //                 }
+
+    //             }
+
+    //         }
+    //     }
+    //     return s.substring(start, start+max);
+    // }
+
+    //Approach2 
+    int maxLen = 0;
+    int start;
 
     public String longestPalindrome(String s) {
-        //Without DP;
-        int max = 0;
-        int start = 0;
         int n = s.length();
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalnidrome(s, i, j)) {
-                    int len = j - i + 1;
-                    if ( len > max) {
-                        max = len;
-                        start = i;
-                    }
-                    
-                }
+        if (n < 2)
+            return s;
 
-            }
+        for (int i = 0; i < n - 1; i++) {
+            expand(s, i, i);
+            expand(s, i, i + 1);
         }
-        return s.substring(start, start+max);
+        return s.substring(start, start + maxLen);
+    }
+
+    public void expand(String s, int begin, int end) {
+        while (begin >= 0 && end < s.length() && s.charAt(begin) == s.charAt(end)) {
+            begin--;
+            end++;
+        }
+        if (maxLen < (end - begin - 1)) {
+            start = begin + 1;
+            maxLen = end - begin - 1;
+        }
     }
 }
