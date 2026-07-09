95f0fc96c091e16822e083249e5d24c4f9e59964
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-07-09 19:05:03 +0530

    Time: 47 ms (36.77%) | Memory: 45.9 MB (82.75%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        //Start 1st pointer
+        for (int i = 1; i < n; i++) {
+            // start 2nd pointer
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (dp[i] < dp[j] + 1)
+                        dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        // Find the the max value in dp array
+        int max = 0;
+        for(int i = 0; i < dp.length; i++)
+           max = Math.max(dp[i],max);
+        return max+1;
+    }
+}
