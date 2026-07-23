5da3c549318fe2139038a7f2846e5c12930737ad
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-07-23 23:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (82.38%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,37 +1,69 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
+
+         if (nums == null || nums.length == 0) {
+          return -1;
+         }
         int high = nums.length - 1;
-        return modifiedBinarySearch(nums, target, low, high);
+        int pivot = findPivotIndex(nums);
+        if(pivot == 0)
+            return binarySearch(nums, target, 0, high);
+        if(target >= nums[0])
+            return binarySearch(nums, target, 0, pivot - 1);
+        return binarySearch(nums, target, pivot, high);
+        
+    }
+
+    private int findPivotIndex(int[] nums) {
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i] )
+                return i;
+        }
+        return 0;
+    }
+
+    private int binarySearch(int[] nums, int target, int low, int high) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (target > nums[mid])
+                low = mid + 1;
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
 
     }
 
-    private int modifiedBinarySearch(int[] nums, int target, int low, int high) {
+    // private int modifiedBinarySearch(int[] nums, int target, int low, int high) {
 
-        if (low > high)
-            return -1;
+    //     if (low > high)
+    //         return -1;
 
-        int mid = low + (high - low) / 2;
+    //     int mid = low + (high - low) / 2;
 
-        if (nums[mid] == target)
-            return mid;
+    //     if (nums[mid] == target)
+    //         return mid;
 
-        //Check left subarray sorted or not
-        if (nums[low] <= nums[mid]) { // Left sub array sorted
-            // If the key is in left half
-            if (nums[low] <= target && target <= nums[mid])
-                return modifiedBinarySearch(nums, target, low, mid - 1);
-            else
-                return modifiedBinarySearch(nums, target, mid + 1, high);
-        }
+    //     //Check left subarray sorted or not
+    //     if (nums[low] <= nums[mid]) { // Left sub array sorted
+    //         // If the key is in left half
+    //         if (nums[low] <= target && target <= nums[mid])
+    //             return modifiedBinarySearch(nums, target, low, mid - 1);
+    //         else
+    //             return modifiedBinarySearch(nums, target, mid + 1, high);
+    //     }
 
-        else { // Right sub array sorted
+    //     else { // Right sub array sorted
 
-            //if the key is in the right half
-            if (nums[mid] <= target && target <= nums[high])
-                return modifiedBinarySearch(nums, target, mid + 1, high);
-            else
-                return modifiedBinarySearch(nums, target, low, mid - 1);
-        }
-    }
+    //         //if the key is in the right half
+    //         if (nums[mid] <= target && target <= nums[high])
+    //             return modifiedBinarySearch(nums, target, mid + 1, high);
+    //         else
+    //             return modifiedBinarySearch(nums, target, low, mid - 1);
+    //     }
+    // }
 }
