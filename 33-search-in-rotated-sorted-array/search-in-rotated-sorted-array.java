037f6db0e294ad4efc19349afb8ec7240e2101cc
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-07-23 19:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (27.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -2,34 +2,36 @@ class Solution {
     public int search(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
-        return modifiedBinarySearch(nums, target, low , high);
-        
+        return modifiedBinarySearch(nums, target, low, high);
+
     }
 
-    private int modifiedBinarySearch(int[] nums, int target, int low , int high){
+    private int modifiedBinarySearch(int[] nums, int target, int low, int high) {
 
-        if(low > high)
+        if (low > high)
             return -1;
 
-        int mid = low + (high-low)/2;
+        int mid = low + (high - low) / 2;
 
-        if(nums[mid] == target)
+        if (nums[mid] == target)
             return mid;
 
         //Check left subarray sorted or not
-        if(nums[low] <= nums[mid]){ // Left sub array sorted
-
-            if(nums[low] <= target  && target <= nums[mid])
-                return modifiedBinarySearch(nums, target, low , mid-1);
+        if (nums[low] <= nums[mid]) { // Left sub array sorted
+            // If the key is in left half
+            if (nums[low] <= target && target <= nums[mid])
+                return modifiedBinarySearch(nums, target, low, mid - 1);
             else
-                return modifiedBinarySearch(nums,target,mid+1,high);
-        } 
+                return modifiedBinarySearch(nums, target, mid + 1, high);
+        }
 
-        else{ // Right sub array sorted
-            if(nums[mid] <= target && target <= nums[high])
-                return modifiedBinarySearch(nums, target, mid+1, high);
+        else { // Right sub array sorted
+
+            //if the key is in the right half
+            if (nums[mid] <= target && target <= nums[high])
+                return modifiedBinarySearch(nums, target, mid + 1, high);
             else
-                return modifiedBinarySearch(nums, target, low, mid-1);
-        }       
+                return modifiedBinarySearch(nums, target, low, mid - 1);
+        }
     }
 }
