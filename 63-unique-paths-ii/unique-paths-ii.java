107d4e273629e6b3cf4744dc3445d9d524e84841
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-07-30 19:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (10.98%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,29 +1,52 @@
 class Solution {
-    int m, n;
-    int[][] dp = new int[101][101];
+    // int m, n;
+    // int[][] dp = new int[101][101];
+
+    // public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+    //     return solve(obstacleGrid, 0, 0);
+    // }
+
+    // public int solve(int[][] obstacleGrid, int i, int j) {
+    //     m = obstacleGrid.length;
+    //     n = obstacleGrid[0].length;
+
+    //     if (i >= m || j >= n || obstacleGrid[i][j] != 0)
+    //         return 0;
+
+    //     if (dp[i][j] != 0)
+    //         return dp[i][j];
+
+    //     if (i == m - 1 && j == n - 1)
+    //         return 1;
+
+    //     int right = solve(obstacleGrid, i, j + 1);
+    //     int down = solve(obstacleGrid, i + 1, j);
+
+    //     return dp[i][j] = right + down;
+
+    // }
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
 
-        return solve(obstacleGrid, 0, 0);
-    }
-
-    public int solve(int[][] obstacleGrid, int i, int j) {
-        m = obstacleGrid.length;
-        n = obstacleGrid[0].length;
-
-        if (i >= m || j >= n || obstacleGrid[i][j] != 0)
+        if (obstacleGrid[0][0] == 1)
             return 0;
 
-        if (dp[i][j] != 0)
-            return dp[i][j];
-            
-        if (i == m - 1 && j == n - 1)
-            return 1;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
 
-        int right = solve(obstacleGrid, i, j + 1);
-        int down = solve(obstacleGrid, i + 1, j);
-
-        return dp[i][j] = right + down;
-
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1)
+                    continue;
+                if (i - 1 >= 0)
+                    dp[i][j] += dp[i - 1][j];
+                if (j - 1 >= 0)
+                    dp[i][j] += dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
