623bb618312991a8c41d194512b040ee4fd3782e
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-04 18:57:37 +0530

    Time: 117 ms (93.58%) | Memory: 43 MB (60.91%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        char[] word_arr = word.toCharArray();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word_arr[0] && find(board, i, j, word_arr, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean find(char[][] board, int i, int j, char[] word_arr, int idx) {
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length ||
+                board[i][j] == '$' || board[i][j] != word_arr[idx])
+            return false;
+        if (idx == word_arr.length - 1)
+            return true;
+        char ch = board[i][j];
+        board[i][j] = '$';
+        boolean result = find(board, i + 1, j, word_arr, idx + 1) ||
+                find(board, i - 1, j, word_arr, idx + 1) ||
+                find(board, i, j + 1, word_arr, idx + 1) ||
+                find(board, i, j - 1, word_arr, idx + 1);
+        board[i][j] = ch;
+        return result;
+
+    }
+}
