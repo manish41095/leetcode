11dc193e2fd9f78d3b5a2c125ece273bcb96f758
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-07 10:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (16.81%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        int n  = matrix.length;
+
+        for(int i = 0; i < (n+1)/2 ; i++){
+            for(int j = 0; j < n/2 ; j++){
+                int temp = matrix[n-1-j][i]; // bottom left
+
+                matrix[n-1-j][i] = matrix[n-1-i][n-j-1]; // copy bottom right to bottom left
+
+                matrix[n-1-i][n-j-1] = matrix[j][n-1-i]; //Copy top right to bottom right
+
+                matrix[j][n-1-i] = matrix[i][j]; // copy top left to top right;
+
+                matrix[i][j] = temp ; // copy bottom left to top left
+            }
+        }
+
+        
+    }
+}
