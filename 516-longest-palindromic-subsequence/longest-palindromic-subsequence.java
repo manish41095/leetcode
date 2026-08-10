229776f1a68a0b0c6b155ef3acfa553b0d72f21d
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-10 19:05:29 +0530

    Time: 31 ms (79.79%) | Memory: 164.7 MB (5.18%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,12 +1,12 @@
 class Solution {
+    int[][] dp = new int[1001][1001];
     public int longestPalindromeSubseq(String s) {
         int n = s.length();
-        int[][] dp = new int[n][n];
-       
-        return solve(s, 0, n - 1, dp);
+        
+        return solve(s, 0, n - 1);
     }
 
-    public int solve(String s, int i, int j, int[][] dp) {
+    public int solve(String s, int i, int j) {
         if (i > j)
             return 0;
         if (i == j)
@@ -14,9 +14,9 @@ class Solution {
         if (dp[i][j] != 0)
             return dp[i][j];
         if (s.charAt(i) == s.charAt(j)) {
-            return dp[i][j] = 2 + solve(s, i + 1, j - 1, dp);
+            return dp[i][j] = 2 + solve(s, i + 1, j - 1);
         } else {
-            return dp[i][j] = Math.max(solve(s, i, j - 1, dp), solve(s, i + 1, j, dp));
+            return dp[i][j] = Math.max(solve(s, i, j - 1), solve(s, i + 1, j));
         }
     }
 }
