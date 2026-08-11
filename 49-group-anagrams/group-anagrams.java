fbd1a0008c7a01e6d312634601dda6d827e9826f
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-11 19:00:58 +0530

    Time: 6 ms (99.28%) | Memory: 49.8 MB (44.20%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,37 +1,37 @@
 class Solution {
-    public String getCharCount(String s){
-        int[] freq = new int[26];
-        
-        for(char c : s.toCharArray()){
-            freq[c - 'a' ]++;
-        }
+    // public String getCharCount(String s){
+    //     int[] freq = new int[26];
 
-        StringBuilder sb = new StringBuilder("");
-        char c = 'a';
-        for(int i : freq){
-            sb.append(c);
-            sb.append(i);
-            c++;
-        }
-        return sb.toString();
-    }
+    //     for(char c : s.toCharArray()){
+    //         freq[c - 'a' ]++;
+    //     }
+
+    //     StringBuilder sb = new StringBuilder("");
+    //     char c = 'a';
+    //     for(int i : freq){
+    //         sb.append(c);
+    //         sb.append(i);
+    //         c++;
+    //     }
+    //     return sb.toString();
+    // }
     public List<List<String>> groupAnagrams(String[] strs) {
 
         Map<String, List<String>> res = new HashMap<>();
 
-        for(int i = 0; i < strs.length ; i++){
-            //char[] strToChar = strs[i].toCharArray();
-           // Arrays.sort(strToChar);
-          //  String sortedStr = new String(strToChar);
-            String sortedStr = getCharCount(strs[i]);
-            if(res.containsKey(sortedStr))
-            res.get(sortedStr).add(strs[i]);
-            else{
+        for (int i = 0; i < strs.length; i++) {
+            char[] strToChar = strs[i].toCharArray();
+            Arrays.sort(strToChar);
+            String sortedStr = new String(strToChar);
+            //String sortedStr = getCharCount(strs[i]);
+            if (res.containsKey(sortedStr))
+                res.get(sortedStr).add(strs[i]);
+            else {
                 List<String> str = new ArrayList<>();
                 str.add(strs[i]);
                 res.put(sortedStr, str);
             }
-         }
+        }
         return new ArrayList<>(res.values());
     }
 }
