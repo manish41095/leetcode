bdd7004567d0de3a5b108b141f01c0094dda51f9
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-12 18:55:04 +0530

    Time: 1 ms (83.68%) | Memory: 42.5 MB (88.01%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,78 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        // Map<Character, String> map = new HashMap<>();
+        // String[] values = s.split(" ");
+        // if(pattern.length()!=values.length){
+        //     return false;
+        // }
+        // for(int i = 0; i < pattern.length(); i++){
+        //     char key = pattern.charAt(i);
+        //     String value = values[i];
+        //     if(!map.containsKey(key)){
+        //         if(!map.containsValue(value)){
+        //           map.put(key,value);
+        //         }
+        //         else
+        //             return false;
+        //     }
+        //     else{
+        //         String mapValue = map.get(key);
+        //         if(!mapValue.equals(value)){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        
+
+     
+       Map<Character, String> map = new HashMap<>();
+       String[] values = s.split(" ");
+
+       if(pattern.length()!= values.length)  return false;
+
+       for(int i = 0 ; i < values.length; i++){
+        char key = pattern.charAt(i);
+        String value = values[i];
+        if(!map.containsKey(key)){
+            if(!map.containsValue(value)){
+                map.put(key,value);
+            }
+            else{
+                for(Map.Entry<Character, String> entry : map.entrySet()){
+                    if(entry.getValue().equals(value)){
+                        char entryKey = entry.getKey();
+                        if(entryKey!=key)
+                        return false;
+                    }
+                }
+            }
+
+        }
+        else{
+            String getValue = map.get(key);
+            if(!value.equals(getValue))
+              return false;
+
+        }
+
+       }
+       return true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
