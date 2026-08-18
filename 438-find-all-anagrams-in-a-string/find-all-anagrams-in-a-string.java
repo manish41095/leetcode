58776714cbb13756f1951b8190543992cc2341b1
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-18 17:34:47 +0530

    Time: 11 ms (66.79%) | Memory: 46.5 MB (92.36%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,27 +1,24 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-
-        // Map<Character, Integer> pCount = new HashMap<>();
-        // Map<Character, Integer> sCount = new HashMap<>();
         int[] pCount = new int[26];
         int[] sCount = new int[26];
 
         List<Integer> result = new ArrayList<>();
-
-        // Count the frequency character of string p
+        int pLen = p.length();
+        // Count the frequency of character in string p
         for (char c : p.toCharArray())
             pCount[c - 'a']++;
 
-        // Sliding window and  count the frequency  char of String s
-
+        // Sliding window and  count the frequency  of char in String s
         for (int i = 0; i < s.length(); i++) {
             sCount[s.charAt(i) - 'a']++;
 
+            //Remove the character that is out of the window
             if (i >= p.length())
-                sCount[s.charAt(i - p.length()) - 'a']--;
+                sCount[s.charAt(i - pLen) - 'a']--;
 
             if (Arrays.equals(pCount, sCount)) {
-                result.add(i - p.length() + 1);
+                result.add(i - pLen + 1);
             }
         }
         return result;
