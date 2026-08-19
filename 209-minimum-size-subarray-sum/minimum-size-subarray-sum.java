596dfe636205f2b6c13bc56aa3ed0a91639075ef
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-19 18:16:44 +0530

    Time: 1 ms (99.89%) | Memory: 69 MB (92.58%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,24 +1,40 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        
-        int minLenWindow = Integer.MAX_VALUE;
-        int currSum = 0;
-        int low = 0;
-        int high = 0;
-        
-        while(high < nums.length){
-            currSum += nums[high];
-            high++;
-            
-            while(currSum >= target){
-                int currWindowSize = high - low;
-                minLenWindow = Math.min(currWindowSize, minLenWindow);
 
-                currSum -= nums[low];
-                low++;
+        int result = Integer.MAX_VALUE;
+        int currSum = 0;
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+
+            while (currSum >= target) {
+                result = Math.min(result, i - left + 1);
+                currSum -= nums[left];
+                left++;
             }
         }
 
-        return minLenWindow == Integer.MAX_VALUE ? 0 : minLenWindow;
+        return result == Integer.MAX_VALUE ? 0 : result;
+
+        // int minLenWindow = Integer.MAX_VALUE;
+        // int currSum = 0;
+        // int low = 0;
+        // int high = 0;
+
+        // while(high < nums.length){
+        //     currSum += nums[high];
+        //     high++;
+
+        //     while(currSum >= target){
+        //         int currWindowSize = high - low;
+        //         minLenWindow = Math.min(currWindowSize, minLenWindow);
+
+        //         currSum -= nums[low];
+        //         low++;
+        //     }
+        // }
+
+        // return minLenWindow == Integer.MAX_VALUE ? 0 : minLenWindow;
     }
 }
