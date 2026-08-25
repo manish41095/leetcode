5dd30eee98b31d4b8cc4f0196086cd190e970cf8
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-25 22:45:15 +0530

    Time: 2 ms (61.48%) | Memory: 45 MB (37.75%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -26,9 +26,13 @@ class Solution {
 
         int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
+
+        // Count total frequencies of each number in the array
         for (int num : nums)
             map.put(num, map.getOrDefault(num, 0) + 1);
 
+        // Case 1: Subarray size is 1
+        // Every element forms its own subarray. We need the max element with frequency 1.
         if (k == 1) {
             int max = -1;
             for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
@@ -39,10 +43,12 @@ class Solution {
             return max;
         }
 
-        // One subarray of size n so we need to find max element in the array
         //if any element appear more than one time in same sub array that will not be conidered
         //two  apperance its conside only one apperance it will conside two only if it 
         //appear   in two different sub array
+
+        // Case 2: Subarray size equals the whole array length
+        // There is only 1 subarray. Every element appears in it exactly once.
         if (k == n) {
             int max = -1;
             for (int num : nums)
@@ -51,27 +57,22 @@ class Solution {
             return max;
         }
 
-        // 2 < k > n
-        // In this case one first and last element have chance to appear only once rest of
-        // element will appear more than one time in k length of sub array.
+        // Case 3: 1 < k < n
+        // Only the first and last elements can appear in exactly one subarray of size k.
+        // Elements in the middle will always appear in multiple subarrays.
 
         int firstNum = nums[0];
-        int lastNum = nums[n-1];
-        int firstCount = 0;
-        int lastCount = 0;
+        int lastNum = nums[n - 1];
         int max = -1;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(entry.getKey() == firstNum)
-               firstCount = entry.getValue();
-             if(entry.getKey() == lastNum)
-               lastCount = entry.getValue(); 
 
-             if(firstCount == 1)
-               max = Math.max(firstNum, max);   
+        int firstCount = map.get(firstNum);
+        int lastCount = map.get(lastNum);
 
-              if(lastCount == 1)
-               max = Math.max(lastNum, max);      
-        }
+        if (firstCount == 1)
+            max = Math.max(firstNum, max);
+
+        if (lastCount == 1)
+            max = Math.max(lastNum, max);
 
         return max;
 
