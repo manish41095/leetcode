4a86a0d3f31ddac6afe7898eb0b0390418ccd778
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-08-26 22:49:23 +0530

    Time: 40 ms (14.41%) | Memory: 68.2 MB (91.60%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -1,26 +1,40 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int n = nums.length;
-        int i = 0;
-        int j = n-1;
-        int count = 0;
-        Arrays.sort(nums);
-        while(i < j){
-            int sum = nums[i] + nums[j];
-            if(sum == k){
-                i++;
-                j--;
-                count++;
-            }
-            else if(sum < k){
-                i++;
-            }
-            else{
-                j--;
-            }
-        }
+        // approach1 - two point TC O(nlogn)
+        // int i = 0;
+        // int j = n-1;
+        // int count = 0;
+        // Arrays.sort(nums);
+        // while(i < j){
+        //     int sum = nums[i] + nums[j];
+        //     if(sum == k){
+        //         i++;
+        //         j--;
+        //         count++;
+        //     }
+        //     else if(sum < k){
+        //         i++;
+        //     }
+        //     else{
+        //         j--;
+        //     }
+        // }
 
+        // return count;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for(int i = 0 ; i < n ; i++){
+          if(map.containsKey(k - nums[i]) && map.get(k - nums[i])!=0){
+             count++;
+             map.put(k - nums[i], map.get(k - nums[i])-1);
+          }
+         
+          else
+          map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+
+        }
         return count;
-        
     }
 }
