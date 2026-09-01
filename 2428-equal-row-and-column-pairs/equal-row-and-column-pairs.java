579cfa074f5f6bbbf6b385e34841f5a2d5ab8b2a
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-09-01 18:39:49 +0530

    Time: 20 ms (75.83%) | Memory: 54.2 MB (43.58%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -3,23 +3,47 @@ class Solution {
         int count = 0;
         int rowSize = grid.length;
         int colSize = grid[0].length;
+        //Brute Force Approach
+        // for (int row = 0; row < rowSize; row++) {
+        //     for (int col = 0; col < colSize; col++) {
+        //         boolean isEqual = true;
+
+        //         for (int i = 0; i < rowSize; i++) {
+        //             if (grid[row][i] != grid[i][col]) {
+        //                 isEqual = false;
+        //                 break;
+        //             }
+        //         }
+
+        //         if (isEqual) {
+        //             count++;
+        //         }
+
+        //     }
+        // }
+
+        //Optimzed approach using map
+
+        Map<List<Integer>, Integer> map = new HashMap<>();
 
         for (int row = 0; row < rowSize; row++) {
+            List<Integer> rowMap = new ArrayList<>();
             for (int col = 0; col < colSize; col++) {
-                boolean isEqual = true;
-
-                for (int i = 0; i < rowSize; i++) {
-                    if (grid[row][i] != grid[i][col]) {
-                        isEqual = false;
-                        break;
-                    }
-                }
-                
-                if (isEqual) {
-                    count++;
-                }
-
+                rowMap.add(grid[row][col]);
             }
+            map.put(rowMap, map.getOrDefault(rowMap, 0) + 1);
+        }
+
+        for (int col = 0; col < colSize; col++) {
+            List<Integer> colMap = new ArrayList<>();
+            for (int row = 0; row < rowSize; row++) {
+                colMap.add(grid[row][col]);
+            }
+
+            if (map.containsKey(colMap)) {
+                count += map.get(colMap);
+            }
+
         }
 
         return count;
