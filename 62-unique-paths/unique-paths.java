fabbdcd223abd5f6dc46b71860f95ed2f4969c81
manish41095 <42186970+manish41095@users.noreply.github.com>
2026-09-16 21:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (28.66%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -3,8 +3,11 @@ class Solution {
         int[][] dp = new int[m][n];
         for(int i = 0; i < m; i++)
             for(int j = 0 ; j < n ; j++)
+                // Fill the first row and first column with 1s
                 if(i == 0 || j == 0)
                     dp[i][j] = 1;
+
+                // Fill the rest of the table    
                 else
                     dp[i][j] = dp[i-1][j] + dp[i][j-1];    
         
